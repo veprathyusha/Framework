fa60bb1ca981e0e017c619f995cbebe61a273d0d
Gargeyi <Gargeyi@DESKTOP-9TVFF7R>
2023-11-08 11:15:41 +0530

    added test data file

--- src/test/java/script/ValidLogin.java
+++ src/test/java/script/ValidLogin.java
@@ -4,26 +4,30 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import generic.BaseTest2;
+import generic.Excel;
 import page.DashboardPage;
 import page.LoginPage;
 
 public class ValidLogin extends BaseTest2{
 	
-	@Test(priority = 1)
-	public void testValidLogin()
-	{
-//		1. enter valid un
-		LoginPage loginPage=new LoginPage(driver);
-		loginPage.setUserName("Admin");
-//		2. enter valid pw
-		loginPage.setPassword("admin123");
-//		3. click on login button
-		loginPage.clickLoginButton();
-//		4. home page should be displayed
-		DashboardPage dashboardPage=new DashboardPage(driver);
-		boolean result = dashboardPage.verifyDashBoardPageIsDisplayed(wait);
-		Assert.assertTrue(result);
-	}
+	
+//		@Test(priority = 1)
+		public void testValidLogin()
+		{
+			String un = Excel.getData("./data/input.xlsx","ValidLogin", 1, 0);
+			String pw = Excel.getData("./data/input.xlsx","ValidLogin", 1, 1);
+//			1. enter valid un
+			LoginPage loginPage=new LoginPage(driver);
+			loginPage.setUserName(un);
+//			2. enter valid pw
+			loginPage.setPassword(pw);
+//			3. click on login button
+			loginPage.clickLoginButton();
+//			4. home page should be displayed
+			DashboardPage dashboardPage=new DashboardPage(driver);
+			boolean result = dashboardPage.verifyDashBoardPageIsDisplayed(wait);
+			Assert.assertTrue(result);
+		}
 	
 
 
